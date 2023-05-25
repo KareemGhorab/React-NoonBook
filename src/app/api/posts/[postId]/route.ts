6932b5592653d40be93185b31507fa819280b11c
Kareem Ghorab <kareemghorab3@gmail..com>
2023-05-25 21:27:18 +0300

    [+] Api logging

--- src/app/api/posts/[postId]/route.ts
+++ src/app/api/posts/[postId]/route.ts
@@ -15,6 +15,7 @@ export async function GET(
 
 		return NextResponse.json({ post })
 	} catch (error) {
+		console.log(error)
 		return NextResponse.json({ error })
 	}
 }
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -15,6 +15,7 @@ export async function GET(request: Request) {
 
 		return NextResponse.json({ posts })
 	} catch (error) {
-		return NextResponse.json({ error })
+		console.log(error)
+		return NextResponse.json({ posts: [] })
 	}
 }
